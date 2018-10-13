599d77fe7648fe8e85a9e6cc7bebb01c110d5b9b
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-10-13 15:58:44 -0700

    Enable class properties
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 
 class App extends Component {
+  state = {
+    name: 'Gordon'
+  };
+
   render() {
     return (
       <div>
         <h1>Hello World</h1>
+        <h2>My name is {this.state.name}</h2>
       </div>
     );
   }
