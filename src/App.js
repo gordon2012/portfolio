bb2d20dfa0a9b922d2834e87a906d322edae36ba
Gordon Doskas <gordon2012@users.noreply.github.com>
2018-10-14 11:43:31 -0700

    Add modal
    Signed-off-by: Gordon Doskas <gordon2012@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -4,28 +4,74 @@ import { render } from 'react-dom';
 import Container from './Container';
 import Project from './Project';
 
-export const baseUrl = 'https://gordonscampinggear.com/img/';
+export const baseUrl = 'gordonscampinggear.com';
 
 class App extends Component {
   state = {
-    sections: [
+    activeModal: -1,
+
+    projects: [
+      { name: 'Responsive Web Design', heading: true },
       {
-        name: 'Responsive Web Design',
-        projects: [
-          { name: 'Tribute', image: 'tribute.png' },
-          { name: 'Survey Form', image: 'survey.png' },
-          { name: 'Product Landing Page', image: 'product.png' }
+        name: 'Tribute',
+        images: ['tribute.png', 'product.png'],
+        url: 'tribute',
+        repo: 'fcc-tribute',
+        copy: [
+          'Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum.',
+          'Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum.',
+          'Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum.',
+          'Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum.',
+          'Lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum, lorem ipsum.'
         ]
       },
       {
-        name: 'Front End Libraries',
-        projects: [
-          { name: 'Random Quote Machine', image: 'quote.png' },
-          { name: 'Markdown Previewer', image: 'markdown.png' }
-        ]
-      }
+        name: 'Survey Form',
+        images: ['survey.png'],
+        url: 'survey',
+        repo: 'fcc-survey'
+      },
+      {
+        name: 'Product Landing Page',
+        images: ['product.png'],
+        url: 'product',
+        repo: 'fcc-product'
+      },
+      { name: 'Empty Section in Middle', heading: true },
+      { name: 'Front End Libraries', heading: true },
+      {
+        name: 'Random Quote Machine',
+        images: ['quote.png'],
+        url: 'quote',
+        repo: 'fcc-quote'
+      },
+      {
+        name: 'Markdown Previewer',
+        images: ['markdown.png'],
+        url: 'markdown',
+        repo: 'fcc-markdown'
+      },
+      { name: 'Empty Section at End', heading: true }
     ]
   };
+
+  handleProjectClick = event => {
+    if (this.state.activeModal > -1) {
+      return;
+    }
+    document.body.classList.add('modal-open');
+    this.setState({
+      activeModal: +event.target.dataset.index
+    });
+  };
+
+  handleCancelClick = () => {
+    document.body.classList.remove('modal-open');
+    this.setState({
+      activeModal: -1
+    });
+  };
+
   render() {
     return (
       <div id="home">
@@ -56,20 +102,36 @@ class App extends Component {
           <section id="work">
             <Container w={1200}>
               <h1>Work</h1>
-              {this.state.sections.map(section => (
-                <div key={section.name} className="project-section">
-                  <h2>{section.name} Projects</h2>
-                  <div className="project-grid">
-                    {section.projects.map(project => (
+
+              <div className="project-grid">
+                {this.state.projects.map((project, i, a) => {
+                  if (project.heading) {
+                    if (i + 1 === a.length || a[i + 1].heading) {
+                      return null;
+                    } else {
+                      return (
+                        <h2 key={project.name} className="project-heading">
+                          {project.name}
+                        </h2>
+                      );
+                    }
+                  } else
+                    return (
                       <Project
                         key={project.name}
                         name={project.name}
-                        image={project.image}
+                        images={project.images}
+                        url={project.url}
+                        repo={project.repo}
+                        copy={project.copy}
+                        handleProjectClick={this.handleProjectClick}
+                        handleCancelClick={this.handleCancelClick}
+                        index={i}
+                        showModal={this.state.activeModal === i}
                       />
-                    ))}
-                  </div>
-                </div>
-              ))}
+                    );
+                })}
+              </div>
             </Container>
           </section>
         </main>
--- src/Project.js
+++ src/Project.js
@@ -1,16 +1,76 @@
 import React from 'react';
 import { baseUrl } from './App';
 
+import Modal from './Modal';
+
 const Project = props => {
+  const images = props.images.map(image => `https://${baseUrl}/img/${image}`);
+
   return (
-    <div className="project card">
+    <button
+      className="project card"
+      onClick={props.handleProjectClick}
+      data-index={props.index}
+    >
+      <div className="project-hover">
+        <span aria-label="show modal" role="img">
+          🔍
+        </span>
+      </div>
       <div className="project-header">
         <h1>{props.name}</h1>
       </div>
       <div className="project-image">
-        <img src={`${baseUrl}${props.image}`} alt={props.name} />
+        <img src={images[0]} alt={props.name} />
       </div>
-    </div>
+      {props.showModal ? (
+        <Modal>
+          <div className="project-modal">
+            <button className="close-button" onClick={props.handleCancelClick}>
+              <span>✖</span>
+            </button>
+            <h1>{props.name}</h1>
+
+            <div className="project-modal-images">
+              {images.map((image, i) => (
+                <img key={image} src={image} alt={`${props.name} ${i + 1}`} />
+              ))}
+            </div>
+
+            <div className="project-modal-copy">
+              {props.copy && props.copy.map((copy, i) => <p key={i}>{copy}</p>)}
+            </div>
+
+            <div className="project-modal-buttons">
+              <div>
+                <a
+                  href={`https://${props.url}.${baseUrl}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="https://imgplaceholder.com/40x40/transparent/333/fa-external-link"
+                    alt="Visit Demo"
+                  />
+                </a>
+              </div>
+              <div>
+                <a
+                  href={`https://github.com/gordon2012/${props.repo}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <img
+                    src="https://imgplaceholder.com/40x40/transparent/333/fa-github"
+                    alt="View Repository"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </Modal>
+      ) : null}
+    </button>
   );
 };
 
